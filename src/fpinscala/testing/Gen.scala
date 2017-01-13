b0ca69c33d07cdd52e7d7c059fa22cd307606e13
Alice Fuzier Cayla <alice.fuzier-cayla@nature.com>
2017-01-13 09:37:47 +0000

    Copying some of the code explained up until exercise 8.16

--- src/fpinscala/testing/Gen.scala
+++ src/fpinscala/testing/Gen.scala
@@ -1,24 +1,17 @@
 package fpinscala.testing
 
-import fpinscala.laziness.Stream
-import fpinscala.state._
-import fpinscala.testing.Prop.{Falsified, _}
+import java.util.concurrent.{ExecutorService, Executors}
 
+import fpinscala.laziness.Stream
+import fpinscala.parallelism.Par
+import fpinscala.parallelism.Par.Par
+import fpinscala.state._
+import fpinscala.testing.Gen._
+import fpinscala.testing.Prop.{Falsified, _}
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
-
-//
-//trait Prop {
-//  def check: Boolean
-//  def &&(p: Prop): Prop = new Prop {
-//    def check: Boolean = this.check && p.check
-//  }
-//}
-
-
-
 
 case class Prop(run: (MaxSize,TestCases,RNG) => Result){
   def &&(p: Prop) = Prop {
@@ -98,6 +91,64 @@ object Prop {
         }).toList.reduce(_ && _)
       prop.run(max,n,rng)
   }
+
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = RNG.Simple(System.currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Proved =>
+        println(s"+ OK, proved property.")
+    }
+
+  val ES: ExecutorService = Executors.newCachedThreadPool
+
+  val p1 = Prop.forAll(unit(Par.unit(1)))(i =>
+    Par.map(i)(_ + 1)(ES).get == Par.unit(2)(ES).get)
+
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Passed else Falsified("()", 0)
+  }
+
+  val p2 = check {
+    val p = Par.map(Par.unit(1))(_ + 1)
+    val p2 = Par.unit(2)
+    p(ES).get == p2(ES).get
+  }
+
+  def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] =
+    Par.map2(p,p2)(_ == _)
+
+  val p3 = check {
+    equal (
+      Par.map(Par.unit(1))(_ + 1),
+      Par.unit(2)
+    ) (ES) get
+  }
+
+  val S = weighted(
+    choose(1,4).map(Executors.newFixedThreadPool) -> .75,
+    unit(Executors.newCachedThreadPool) -> .25) // `a -> b` is syntax sugar for `(a,b)`
+
+  def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S.map2(g)((_,_))) { case (s,a) => f(a)(s).get }
+
+  def checkPar(p: Par[Boolean]): Prop =
+    forAllPar(unit(()))(_ => p)
+
+  def forAllPar2[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case (s,a) => f(a)(s).get }
+
+  def forAllPar3[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll(S ** g) { case s ** a => f(a)(s).get }
+
+  val pint = choose(0,10) map (Par.unit(_))
+  val p4 =
+    forAllPar(pint)(n => equal(Par.map(n)(y => y), n))
 }
 
 object Gen {
@@ -113,29 +164,48 @@ object Gen {
 
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b=> if(b)g1 else g2)
 
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    /* The probability we should pull from `g1`. */
+    val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+
+    Gen(State(RNG.double).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
+  }
+
   def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(x=>listOfN(x,g))
 
   def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(x=>listOfN(x max 1,g))
-}
 
-//trait Gen[A] {
-//  def map[A,B](f: A => B): Gen[B] = ???
-//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-//}
+  object ** {
+    def unapply[A,B](p: (A,B)) = Some(p)
+  }
+}
 
 case class Gen[A](sample: State[RNG,A]) {
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(x=>f(x).sample))
 
-  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(x=> Gen.listOfN(x,this))
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(x=> listOfN(x))
 
   def unsized: SGen[A] = SGen(n=>this)
+
+  def map[B](f: A => B): Gen[B] =
+    Gen(sample.map(f))
+
+  def map2[B,C](g: Gen[B])(f: (A,B) => C): Gen[C] =
+    Gen(sample.map2(g.sample)(f))
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
+  /* A method alias for the function we wrote earlier. */
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+
+
+  def **[B](g: Gen[B]): Gen[(A,B)] =
+    (this map2 g)((_,_))
 }
 
-case class SGen[+A](forSize: Int => Gen[A]){
+case class SGen[A](forSize: Int => Gen[A]){
   def apply(n: Int): Gen[A] = forSize(n)
 }
-
-//trait SGen[+A] {
-//
-//}
 
--- src/fpinscala/testing/hgjfd.sc
+++ src/fpinscala/testing/hgjfd.sc
@@ -2,7 +2,7 @@ import fpinscala.testing.{Gen, Prop}
 
 val smallInt = Gen.choose(1,2)
 
-val prop = Prop.forAll(smallInt)((ns)=>
-  val max = ns.max
-  !ns.exists(_ > max)
+val prop = Prop.forAll(Gen.listOf1(smallInt))((ns) => {
+  ns.sorted.head == ns.min
+}
 )
