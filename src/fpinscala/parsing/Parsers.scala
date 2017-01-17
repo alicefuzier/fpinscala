cf383270cfa98b5293d6e0f61908dde96c76e43a
Alice Fuzier Cayla <alice.fuzier-cayla@nature.com>
2017-01-17 13:54:50 +0000

    Copied code explained in chpater 9 up until exercise 9.1

--- src/fpinscala/parsing/Parsers.scala
+++ src/fpinscala/parsing/Parsers.scala
@@ -1,15 +1,49 @@
 package fpinscala.parsing
 
+import fpinscala.testing.Prop._
+import fpinscala.testing.{Gen, Prop}
+
 import language.higherKinds
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+  def run[A](p: Parser[A])(input: String): Either[ParseError,A]
+
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+  ParserOps[String] = ParserOps(f(a))
+
+  def char(c: Char): Parser[Char] =
+    string(c.toString) map (_.charAt(0))
+
+
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def slice[A](p: Parser[A]): Parser[String]
+  def product[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)]
 
   case class ParserOps[A](p: Parser[A]) {
+    def |[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2) // use `self` to explicitly disambiguate reference to the `or` method on the `trait`
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
 
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def many = self.many(p)
 
+    def slice: Parser[String] = self.slice(p)
+
+    def **[B](p2: => Parser[B]): Parser[(A,B)] =
+      self.product(p,p2)
+    def product[B](p2: => Parser[B]): Parser[(A,B)] =
+      self.product(p,p2)
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
   }
 }
 
